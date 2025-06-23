40ff9a4fad3a272dbb883fab0b59bff73f7644dc
Carmen Yepes <kodasouttakes@gmail.com>
2025-06-23 19:30:07 +0200

    instalaciones

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,32 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>@yield('title', 'Curso Arte Digital')</title>
     <link rel="icon" href="{{ asset('images/logo.svg') }}" type="image/svg+xml">
-
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-
-    <!-- Google Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400;600;700&family=Poppins:wght@400;500;600;700&family=Roboto&display=swap"
-        rel="stylesheet" />
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
-
-
-
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <!-- Owl Carousel JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-
-
-    <!-- CSS externos -->
-
     <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     @vite(['resources/js/app.js'])
 
@@ -40,14 +21,11 @@
 <body>
 
     @include('components.header')
-
     <main>
         @yield('content')
     </main>
 
     @include('components.footer')
-
-    <!-- Bootstrap JS Bundle (Popper + Bootstrap JS) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
